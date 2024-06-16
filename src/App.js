eac80e3170f6352d79af9a08dc2dd139daaf9d9f
nesar <bibizahrasadaf@gmail.com>
2024-06-16 12:25:10 +0430

    "Make a app"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './features/Home/Home';
+import Header from './features/Header/Header';
+import Subreddits from './features/Subreddits/Subreddits';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Home />
+      </main>
+      <aside>
+        <Subreddits />
+      </aside>
+    </>
   );
 }
 
